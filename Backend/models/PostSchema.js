a952d9fca88d58d80d31ae4b1ad8f5ee03f43850
rudresh21 <rudreshsankpal21@gmail.com>
2025-12-31 21:32:52 +0530

    add : post model

---
+++ Backend/models/PostSchema.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+// Post schema
+
+const PostSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    tags: {
+      type: [String],
+    },
+    likesCount: {
+      type: Number,
+      default: 0,
+    },
+    commentsCount: {
+      type: Number,
+      default: 0,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
