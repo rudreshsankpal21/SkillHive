81575f0db5571bb6fe6ea91b13f13c741f1807e0
rudresh21 <rudreshsankpal21@gmail.com>
2025-12-31 21:28:26 +0530

    add : user model

---
+++ Backend/models/UserSchema.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+// User Schema
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    bio: {
+      type: String,
+    },
+    followers: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: 0,
+    },
+    following: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
